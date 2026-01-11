096e3886c30fd93859e33a1a4d180df452fe8dbe
Julien-rd <julien_r@icloud.com>
2026-01-11 16:19:19 +0100

    fixed CP01/ex02

--- CP01/ex02/main.cpp
+++ CP01/ex02/main.cpp
@@ -2,7 +2,15 @@
 
 int main(void)
 {
-    std::string msg = "HI THIS IS BRAIN";
-    std::string *ptr = &msg;
-    
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "original str adress: " << &str << std::endl;
+    std::cout << "stringPTR adress: " << stringPTR << std::endl;
+    std::cout << "stringREF adress: " << &stringREF << std::endl;
+
+    std::cout << "original str value: " << str << std::endl;
+    std::cout << "stringPTR value: " << *stringPTR << std::endl;
+    std::cout << "stringREF value: " << stringREF << std::endl;
 }
